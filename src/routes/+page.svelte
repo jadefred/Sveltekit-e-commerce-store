120d98a7a4924c0b15a8cbb3da8860265e983322
jadefred <jadelaipy@gmail.com>
2022-12-28 22:36:12 +0100

    Show all products in main page and add style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,25 @@
 	export let data: PageData;
 
 	let allProducts = data.allProducts;
+	console.log(allProducts[0]);
 </script>
+
+<div class="text-gray-700 bg-blue-50">
+	<div class="w-11/12 mx-auto text-center">
+		<h1 class="text-3xl font-bold py-8">All Products</h1>
+		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 justify-items-center">
+			<!-- display all products dynamically, each card is a clickable link which link to its url page -->
+			{#each allProducts as product}
+				<div
+					class="flex flex-col items-center gap-y-3 w-5/6 border-2 border-blue-100 bg-white rounded-lg
+				p-5 hover:scale-105 transition-transform"
+				>
+					<p class="font-semibold text-xl">{product.title}</p>
+					<div class="bg-white">
+						<img src={product.image} alt={product.title} class="w-60 h-80 object-contain mx-auto" />
+					</div>
+				</div>
+			{/each}
+		</div>
+	</div>
+</div>
