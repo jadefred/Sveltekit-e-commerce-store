1147b2c21d899057ace153f69e262258337d5fcf
jadefred <jadelaipy@gmail.com>
2022-12-30 23:26:07 +0100

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,15 @@
 </script>
 
 <nav class="flex gap-5 bg-blue-500 text-white font-bold text-2xl py-2 pl-10">
-		<a href="/">Home</a>
-		<a href="/cart">Cart</a>
+	<a href="/">Home</a>
+	<a href="/cart">Cart</a>
 </nav>
 <slot />
+
+<footer class="flex gap-5 bg-blue-500 text-white text-md py-2 pl-10 mt-5">
+	<p>
+		Products' information from <a href="https://fakestoreapi.com/" class="underline"
+			>Fake Store API</a
+		>
+	</p>
+</footer>
