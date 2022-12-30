974f72a9ce4f147fc081d769b705ad350f6e728b
jadefred <jadelaipy@gmail.com>
2022-12-30 23:22:38 +0100

    Fix card size in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,19 +13,14 @@
 		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 justify-items-center">
 			<!-- display all products dynamically, each card is a clickable link which link to its url page -->
 			{#each allProducts as product}
-				<a href={`/${product.id}`}>
-					<div
-						class="flex flex-col items-center gap-y-3 w-5/6 border-2 border-blue-100 bg-white rounded-lg
+				<a
+					href={`/${product.id}`}
+					class="flex flex-col items-center gap-y-3 w-5/6 border-2 border-blue-100 bg-white rounded-lg
 				p-5 hover:scale-105 transition-transform"
-					>
-						<p class="font-semibold text-xl">{product.title}</p>
-						<div class="bg-white">
-							<img
-								src={product.image}
-								alt={product.title}
-								class="w-60 h-80 object-contain mx-auto"
-							/>
-						</div>
+				>
+					<p class="font-semibold text-xl">{product.title}</p>
+					<div class="bg-white">
+						<img src={product.image} alt={product.title} class="w-60 h-80 object-contain mx-auto" />
 					</div>
 				</a>
 			{/each}
