80e5ee7a8afa40de1d9a8d14c8f12402b7ff6c62
jadefred <jadelaipy@gmail.com>
2022-12-30 17:33:07 +0100

    Layout add nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
 	import '../app.css';
 </script>
 
+<nav class="flex gap-5 bg-blue-500 text-white font-bold text-2xl py-2 pl-10">
+		<a href="/">Home</a>
+		<a href="/cart">Cart</a>
+</nav>
 <slot />
