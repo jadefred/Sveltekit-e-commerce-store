19f83cce22c23dc439db3b0ee51f5424a6660974
jadefred <jadelaipy@gmail.com>
2023-01-01 17:50:33 +0100

    Footer stays at bottom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
 </nav>
 <slot />
 
-<footer class="flex gap-5 bg-blue-500 text-white text-md py-2 pl-10 mt-5">
+<footer class="flex gap-5 bg-blue-500 text-white text-md py-2 pl-10 mt-5 fixed bottom-0 w-full">
 	<p>
 		Products' information from <a href="https://fakestoreapi.com/" class="underline"
 			>Fake Store API</a
