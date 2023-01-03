cd91cbf734aa82ec09ea084366711230828b69ec
jadefred <jadelaipy@gmail.com>
2023-01-03 12:16:59 +0100

    Test add product to cart

--- cypress/e2e/productDetail.cy.ts
+++ cypress/e2e/productDetail.cy.ts
@@ -15,8 +15,18 @@ describe('Product details page', () => {
 			cy.get('h2').should('contain', res.body.title);
 			cy.get('img').should('have.attr', 'src').and('equal', res.body.image);
 			cy.get('p').eq(0).should('contain', res.body.description);
-			cy.get('p').eq(1).should('contain', priceFormatter.format(Math.floor(parseInt(res.body.price))));
+			cy.get('p')
+				.eq(1)
+				.should('contain', priceFormatter.format(Math.floor(parseInt(res.body.price))));
 		});
+	});
+
+	it('Show successful message', () => {
+		cy.get('button').click();
+		cy.get('div')
+			.eq(0)
+			.should('be.visible')
+			.should('contain', 'Product has been added to the cart');
 	});
 });
 
